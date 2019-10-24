6424d0d60f1d05a7014b7c380a8a99ffb4dcf434
Dogers <Dogers@users.noreply.github.com>
2019-10-24 23:31:55 +0100

    Update root

--- cmd/root.go
+++ cmd/root.go
@@ -1,38 +1,55 @@
 package cmd
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
-	"os"
-	"strings"
-
 	"github.com/manifoldco/promptui"
-	"github.com/xanzy/go-gitlab"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
 )
 
-func main() {
-	var gitlabToken string
-	var err error
+var gitlabToken string
+var masterGroupID int
+var err error
 
-	gitlabToken = ""
-	masterGroupID := ""
+var rootCmd = &cobra.Command{
+	Use:   "hugo",
+	Short: "Hugo is a very fast static site generator",
+	Long: `A Fast and Flexible Static Site Generator built with
+                love by spf13 and friends in Go.
+                Complete documentation is available at http://hugo.spf13.com`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
 
-	validate10 := func(input string) error {
-		if len(input) < 10 {
-			return errors.New("input must have more than 10 characters")
-		}
-		return nil
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+}
 
-	validate7 := func(input string) error {
-		if len(input) < 7 {
-			return errors.New("input must have more than 7 characters")
-		}
-		return nil
-	}
+func init() {
+	cobra.OnInitialize(initConfig)
 
-	// Check required vars!
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gitlab_cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.PersistentFlags().Int("gid", masterGroupID, "The topmost group ID to recurse from")
+}
+
+func initConfig() {
+	viper.SetEnvPrefix("gitlab_cli")
+	viper.AutomaticEnv()
+
+	gitlabToken = viper.GetString("token")
 	if gitlabToken == "" {
 		prompt := promptui.Prompt{
 			Label:    "GitLab API token",
@@ -48,10 +65,11 @@ func main() {
 		}
 	}
 
+	masterGroupID = viper.GetInt("gid")
 	if masterGroupID == "" {
 		prompt := promptui.Prompt{
 			Label:    "Base group ID",
-			Validate: validate7,
+			Validate: validate10,
 			Default:  "1111111",
 		}
 
@@ -62,27 +80,11 @@ func main() {
 			return
 		}
 	}
+}
 
-	gitlabClient := gitlab.NewClient(nil, gitlabToken)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("GL> ")
-		scanner.Scan()
-		command := scanner.Text()
-
-		switch strings.ToLower(command) {
-		case "help":
-			//printMenu()
-		case "exit":
-			os.Exit(0)
-		case "listusers":
-			getSubGroups(gitlabClient, masterGroupID)
-		case "":
-		default:
-			fmt.Fprintf(os.Stdout, "Unknown command: '%s'\n", command)
-			fmt.Println("Type 'help' for available commands")
-			fmt.Println()
-		}
+func validate10(input string) error {
+	if len(input) < 10 {
+		return errors.New("input must have more than 10 characters")
 	}
+	return nil
 }
