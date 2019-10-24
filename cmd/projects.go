d83f24fbdfa8d91997fd725edfd177c11898019e
Dogers <Dogers@users.noreply.github.com>
2019-10-24 21:52:42 +0100

    Combine projects and projects-list and move to cmd

--- cmd/projects.go
+++ cmd/projects.go
@@ -1,14 +1,37 @@
-package main
+package cmd
 
 import (
 	"fmt"
 	"log"
 	"os"
-
 	"text/tabwriter"
 
 	"github.com/xanzy/go-gitlab"
 )
+
+func getSubGroups(gitlabClient *gitlab.Client, mastergroupid string) {
+
+	lSGOpts := &gitlab.ListSubgroupsOptions{}
+	groups, _, err := gitlabClient.Groups.ListSubgroups(mastergroupid, lSGOpts)
+
+	if err != nil {
+		log.Fatalf("Error: %o", err)
+	}
+
+	for _, group := range groups {
+
+		getMembersOfGroup(gitlabClient, group)
+
+		subgroups, _, err := gitlabClient.Groups.ListSubgroups(group.ID, lSGOpts)
+		if err != nil {
+			log.Fatalf("Error: %o", err)
+		}
+
+		for _, subg := range subgroups {
+			getMembersOfGroup(gitlabClient, subg)
+		}
+	}
+}
 
 func getMembersOfGroup(git *gitlab.Client, group *gitlab.Group) {
 	lgmOpts := &gitlab.ListGroupMembersOptions{}
